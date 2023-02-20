64b7fa2753ddf63ef3a04628b21a86f3a6459180
Sandrex Endino <Sandrex.Cayle.Endino@ph.ey.com>
2023-02-20 13:25:01 +0800

    feat: separated each function for appServices

--- services/functions.js
+++ services/functions.js
@@ -2,11 +2,13 @@ const getListProducts = require('./product/getListProducts.js')
 const getProductDetails = require('./product/getProductDetails')
 const insertProduct = require('./product/insertProduct')
 const updateProduct = require('./product/updateProduct')
+const transactionHistory = require('./product/transactionHistory')
 
 
 module.exports = {
     getListProducts,
     getProductDetails,
     insertProduct,
-    updateProduct
+    updateProduct,
+    transactionHistory
 }
--- services/product/transactionHistory.js
+++ services/product/transactionHistory.js
@@ -1,1 +1,18 @@
-// *
+// * transactionHistory
+
+const mysql = require("../../database/mysql")
+
+const transactionHistory = (req, res) => {
+    // Get query parameters
+
+    // Build the query
+    const query = ` SELECT * FROM purchase_transaction;`
+
+    // Execute the query
+    return  mysql.query(query, (error, result) => {
+        if(error) throw res.json({ message: error })
+        res.json(result)
+    })
+}
+
+module.exports = transactionHistory

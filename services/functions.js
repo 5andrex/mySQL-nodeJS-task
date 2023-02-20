c560d47783db41e949f7e880e3ec1f6cb0f73cd3
Sandrex Endino <Sandrex.Cayle.Endino@ph.ey.com>
2023-02-20 12:05:40 +0800

    feat: separated each function for appServices

---
+++ services/functions.js
@@ -0,0 +1,12 @@
+const getListProducts = require('./product/getListProducts.js')
+const getProductDetails = require('./product/getProductDetails')
+const insertProduct = require('./product/insertProduct')
+const updateProduct = require('./product/updateProduct')
+
+
+module.exports = {
+    getListProducts,
+    getProductDetails,
+    insertProduct,
+    updateProduct
+}

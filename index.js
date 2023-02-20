5b43fa3be0f4338d66c66aaa2c65720e45d36c51
Sandrex Endino <Sandrex.Cayle.Endino@ph.ey.com>
2023-02-20 12:00:45 +0800

    feat: call require packages, create route

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 console.log("hello world")
+
+// Call the required packages
+
+const express = require('express')
+const parser = require('body-parser')
+const routes = require('./router/routes')
+const connection = require('./database/mysql')
+
+// Instantiate the app
+const app = express()
+
+// Set the port
+const port = 5500
+
+// Create the routes
+app.use("/", routes)
+
+// Set the app listener
+app.listen(port, () => {
+    console.log("Listening on port:", port)
+})
+

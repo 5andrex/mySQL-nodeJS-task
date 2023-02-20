8b502a713c316e91b141b099261105273216d7cc
Sandrex Endino <Sandrex.Cayle.Endino@ph.ey.com>
2023-02-20 13:54:47 +0800

    feat: separated each function for appServices

--- services/functions.js
+++ services/functions.js
@@ -3,12 +3,14 @@ const getProductDetails = require('./product/getProductDetails')
 const insertProduct = require('./product/insertProduct')
 const updateProduct = require('./product/updateProduct')
 const transactionHistory = require('./product/transactionHistory')
+const purchaseProduct = require('./product/purchaseProduct')
 
 
 module.exports = {
     getListProducts,
     getProductDetails,
     insertProduct,
-    updateProduct,
-    transactionHistory
+    purchaseProduct,
+    transactionHistory,
+    updateProduct
 }
--- services/product/purchaseProduct.js
+++ services/product/purchaseProduct.js
@@ -1,3 +1,31 @@
 // *purchaseProduct
 
 const mysql = require('../../database/mysql')
+
+const purchaseProduct = (req, res) => {
+    // Get query parameters
+    const { inventoryID, quantity } = req.query
+
+    // Check if inventoryID and quantity are declared
+    let missingField = []
+
+    if(!inventoryID) missingField.push("inventoryID")
+    if(!quantity) missingField.push("quantity")
+
+    if(missingField.length > 0) throw res.json({ message: `Missing required fields: ${missingField.join(", ")}` })
+
+    // Build the query
+    const query = ` CALL purchaseTransaction(${inventoryID}, ${quantity})`
+
+    // Execute the query
+    return mysql.query(query, (error, result) => {
+
+        // Error handling, throw error if there is one
+        if(error) throw res.json({ message: error })
+
+        // Return the result if no error
+        return res.json({ message: result })
+    })
+}
+
+module.exports = purchaseProduct
